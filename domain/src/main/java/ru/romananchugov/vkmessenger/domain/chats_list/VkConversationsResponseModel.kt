4e71380e066a3644af0260e38b8620dcd991c0db
RomanAnchugov <romagn@mail.ru>
2019-07-16 14:21:20 +0200

    Пытаюсь распарсить json, но это жесть, попробую завести moshi

--- domain/src/main/java/ru/romananchugov/vkmessenger/domain/chats_list/VkConversationsResponseModel.kt
+++ domain/src/main/java/ru/romananchugov/vkmessenger/domain/chats_list/VkConversationsResponseModel.kt
@@ -1,5 +1,7 @@
 package ru.romananchugov.vkmessenger.domain.chats_list
 
+
+//WIP
 data class VkConversationsResponseModel(
     val count: Int,
     val items: List<VkConversationWithMessageModel>,
